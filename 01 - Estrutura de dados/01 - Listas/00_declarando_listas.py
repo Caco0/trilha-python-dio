095dafcdff073380c8fd6e246b153829b0a89009
Caco0 <cacotpds@gmail.com>
2024-06-26 14:43:18 -0300

    "Added/modified print statements and list declarations in various Python files related to lists."

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -8,8 +8,10 @@ print(frutas)
 letras = list("python")
 print(letras)
 
+# vai retornar um elemento para cada numero retornado pela função
 numeros = list(range(10))
 print(numeros)
+
 
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
