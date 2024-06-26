dbae50689a6b281880d9b59a47a3af55ba037f41
Caco0 <cacotpds@gmail.com>
2024-06-26 11:24:34 -0300

    update files

---
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -0,0 +1,15 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+# vai criar uma lista e cada letra será um item da lista
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
